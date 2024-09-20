a0ab4773e0b8fe509db05593fbbc700f162bc61f
root <root@LAPTOP-7B54OGNU>
2024-09-20 19:19:34 +0530

    removed comments

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,4 @@
 
-// export const Button = ({text} : {text : string}) => {
-
-//     return <div>
-
-// <button className="py-4 px-10 text-md font-medium text-white focus:outline-none bg-black rounded-full border border-black hover:bg-white hover:text-black focus:z-10 ">{text}</button>
-//     </div>
-// }
 import { Link } from 'react-router-dom';
 export const Button = ({ text, bg = "bg-black", border = "border-black" }: { text: string; bg?: string; border?: string }) => {
     return <div>
--- src/components/CaseStudyCard.tsx
+++ src/components/CaseStudyCard.tsx
@@ -1,23 +1,5 @@
 import { AppCard } from "./AppCard";
 
-
-// export const CaseStudyCard = () => {
-
-
-//     return <div className="flex">
-
-
-//         <div className="border bg-white rounded-3xl px-3 py-3">
-//             <div className="flex flex-col gap-6 lg:grid lg:grid-cols-2 lg:grid-rows-2 xl:flex xl:flex-row xl:gap-4">
-//                 <AppCard image={cat} aspect={"square"} />
-//                 <AppCard image={gtr} aspect={"rectangle"}/>
-//                 <AppCard image={girl2} aspect={"rectangle"}/>
-//             </div>
-//         </div>
-
-//     </div>
-// }
-
 export const CaseStudyCard = ({img} : {img:string}) => {
 
     return <div className="flex">
